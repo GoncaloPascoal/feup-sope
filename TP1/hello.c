77ec08d8b10fd251b7327619b93c57d3561bc38b
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-02-19 09:23:11 +0000

    Finished exercise 4

--- TP1/hello.c
+++ TP1/hello.c
@@ -1,7 +1,20 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello world.\n");
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        printf("Hello %s.\n", argv[1]);
+    }
+    else if (argc == 3) {
+        size_t numTimes = atoi(argv[2]);
+        for (size_t i = 0; i < numTimes; ++i) {
+            printf("Hello %s.\n", argv[1]);
+        }
+    }
+    else {
+        printf("Hello world.\n");
+    }
+    
     return 0;
 }
