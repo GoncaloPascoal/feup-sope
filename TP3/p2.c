1328459b0e51a6b14fee720fac39d63576aefb45
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-03-04 10:06:23 +0000

    Finished question 2

---
+++ TP3/p2.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    printf("1\n");
+
+    if (fork() > 0) {
+        printf("2");
+        printf("3");
+    }
+    else {
+        printf("4");
+        printf("5");
+    }
+    printf("\n");
+
+    return 0;
+}
