6c01352091bc9ec66159c25c97edc377e8e52ddc
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-03-04 10:25:33 +0000

    Finished exercise 4 of TP3

---
+++ TP3/p4b.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    pid_t pid;
+    char str[32];
+
+    pid = fork();
+
+    switch (pid) {
+    case -1:
+        perror("fork");
+        break;
+    case 0: // child
+        strcpy(str, "world!\n");
+        write(STDOUT_FILENO, str, strlen(str));
+        break;
+    default: // parent
+        strcpy(str, "Hello ");
+        write(STDOUT_FILENO, str, strlen(str));
+        break;
+    }
+
+    return 0;
+}
