56828727559f7a342fa71d59621e4b22ab9e6757
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-03-04 00:36:38 +0000

    Added p4b.c

---
+++ TP2/p4b.c
@@ -0,0 +1,22 @@
+// QUESTION 4b - TP2
+// By Goncalo Pascoal (up201806332@fe.up.pt)
+
+#include <stdio.h>
+
+#define MAX_NAME_LEN 50
+
+typedef struct {
+    char name[MAX_NAME_LEN];
+    int grade;
+} StudentData;
+
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        
+    }
+    else {
+        printf("Usage: ./p4a <file>\n");
+    }
+
+    return 0;
+}
