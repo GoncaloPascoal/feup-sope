9abf4eb346ee2ee8864072e6ee225574cb8db201
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-04-01 09:39:21 +0100

    Finished question 4

--- TP5/p4.c
+++ TP5/p4.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #define READ 0
 #define WRITE 1
@@ -19,9 +20,13 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    pid_t pid1 = fork();
+    pid_t pid = fork();
 
-    if (pid1 > 0) { // ls <dir> -laR
+    if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+    else if (pid == 0) {
         close(pipeFD1[READ]);
         dup2(pipeFD1[WRITE], STDOUT_FILENO);
 
@@ -30,40 +35,54 @@ int main(int argc, char* argv[]) {
         perror("execlp");
         exit(1);
     }
-    else if (pid1 == 0) {
-        int pipeFD2[2];
+    else {
+        perror("fork");
+        exit(1);
+    }
 
-        if (pipe(pipeFD2) < 0) {
-            perror("pipe");
-            exit(1);
-        }
+    int pipeFD2[2];
 
-        pid_t pid2 = fork();
+    if (pipe(pipeFD2) < 0) {
+        perror("pipe");
+        exit(1);
+    }
 
-        if (pid2 > 0) { // grep <arg>
-            close(pipeFD1[WRITE]);
-            close(pipeFD2[READ]);
-            dup2(pipeFD2[WRITE], STDOUT_FILENO);
-            dup2(pipeFD1[READ], STDIN_FILENO);
+    close(pipeFD1[WRITE]);
+    pid = fork();
 
-            execlp("grep", "grep", argv[2], NULL);
+    if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+    else if (pid == 0) {
+        close(pipeFD2[READ]);
+        dup2(pipeFD1[READ], STDIN_FILENO);
+        dup2(pipeFD2[WRITE], STDOUT_FILENO);
 
-            perror("execlp");
-            exit(1);
-        }
-        else if (pid2 == 0) { // sort
-            close(pipeFD2[WRITE]);
-            dup2(pipeFD2[READ], STDIN_FILENO);
+        execlp("grep", "grep", argv[2], NULL);
 
-            execlp("sort", "sort", NULL);
+        perror("execlp");
+        exit(1);
+    }
+    else {
+        perror("fork");
+        exit(1);
+    }
 
-            perror("execlp");
-            exit(1);
-        }
-        else {
-            perror("fork");
-            exit(1);
-        }
+    close(pipeFD2[WRITE]);
+    pid = fork();
+
+    if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+    else if (pid == 0) {
+        dup2(pipeFD2[READ], STDIN_FILENO);
+
+        execlp("sort", "sort", NULL);
+
+        perror("execlp");
+        exit(1);
     }
     else {
         perror("fork");
